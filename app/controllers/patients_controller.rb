1d4894d1ef761bf29b675a03cdcfb5b8b5d9165f
Ian <ianlloydwilkinson@gmail.com>
2020-11-19 15:59:16 -0700

    added patient crud actions minus delete/added navbar and scss

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,10 +1,37 @@
 class PatientsController < ApplicationController
+
   def index
+    @patients = Patient.all
   end
 
   def show
+    @patient = Patient.find(params[:id])
   end
 
   def new
+    @patient = Patient.new
   end
+
+  def create
+    @patient = Patient.new(patient_params)
+    if @patient.save
+      redirect_to @patient
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @patient.find(params[:id]).destroy
+    redirect_to patients_path
+  end
+
+  private
+
+  def patient_params
+    params.require(:patient).permit(:first_name, :last_name)
+  end
+
+
+
 end
